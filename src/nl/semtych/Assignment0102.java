5b196910fc560893cd89f52a25078b8ae3c6ee90
BitSemih <semihoren@hotmail.nl>
2019-12-09 19:21:23 +0100

    Added academic solution Big brain time

--- src/nl/semtych/Assignment0102.java
+++ src/nl/semtych/Assignment0102.java
@@ -20,22 +20,78 @@ public class Assignment0102 {
         int tempmin = this.amountofnumbers;
         int tempmax = 1;
         int arr[] = util.createRandomNumberArray(this.amountofnumbers);
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
 
-            if(arr[i] < tempmin){
+            if (arr[i] < tempmin) {
                 tempmin = arr[i];
             }
 
-            if(arr[i] > tempmax){
+            if (arr[i] > tempmax) {
                 tempmax = arr[i];
             }
         }
         System.out.println("Min " + tempmin);
         System.out.println("Max " + tempmax);
+    }
+
+    public void recMinMax(int arr[], int l, int r){
+        if (l < r)
+        {
+            int m = (l+r)/2;
+            System.out.println("LENGTEEEEEEE: " + m);
+            if (m > 1) {
+                recMinMax(arr, l, m);
+                recMinMax(arr, m + 1, r);
+            }
+
+            merge(arr, l, m, r);
+        }
+    }
+
+    void merge(int arr[], int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+
+        int i = 0, j = 0;
+
+        int k = l;
+
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
         }
 
-    public void recMinMax() {
-
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
 
 }
--- src/nl/semtych/Main.java
+++ src/nl/semtych/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	 Assignment0102 ass = new Assignment0102(100);
     ass.seqMinMax();
+        Utility util = new Utility();
+        int [] arr1 = util.createRandomNumberArray(100);
+        util.printArray(arr1);
+        ass.recMinMax(arr1, 0, arr1.length-1);
+        util.printArray(arr1);
+
     }
 }
